e817633b6d8c8b895149937136d91fdb8739a0bf
Santiago Martin <calgaro.santiago@gmail.com>
2018-12-18 17:39:16 -0300

    pagarCOmpra

---
+++ PalcoNet/Comprar/pagarCompra.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PalcoNet.Comprar
+{
+    partial class pagarCompra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxTarjeta = new System.Windows.Forms.TextBox();
+            this.buttonComprar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione medio de Pago";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 49);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(129, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Efectivo en la entrada";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 72);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Tarjeta";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "VISA",
+            "MASTER",
+            "AMERCAN"});
+            this.comboBox1.Location = new System.Drawing.Point(32, 103);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Seleccionar...";
+            // 
+            // textBoxTarjeta
+            // 
+            this.textBoxTarjeta.Enabled = false;
+            this.textBoxTarjeta.Location = new System.Drawing.Point(182, 104);
+            this.textBoxTarjeta.Name = "textBoxTarjeta";
+            this.textBoxTarjeta.Size = new System.Drawing.Size(275, 20);
+            this.textBoxTarjeta.TabIndex = 4;
+            this.textBoxTarjeta.Text = "Nro tarjeta...";
+            // 
+            // buttonComprar
+            // 
+            this.buttonComprar.Location = new System.Drawing.Point(366, 166);
+            this.buttonComprar.Name = "buttonComprar";
+            this.buttonComprar.Size = new System.Drawing.Size(91, 32);
+            this.buttonComprar.TabIndex = 5;
+            this.buttonComprar.Text = "Comprar";
+            this.buttonComprar.UseVisualStyleBackColor = true;
+            this.buttonComprar.Click += new System.EventHandler(this.buttonComprar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(32, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(425, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Nombre en la tarjeta...";
+            // 
+            // pagarCompra
+            // 
+            this.ClientSize = new System.Drawing.Size(478, 212);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonComprar);
+            this.Controls.Add(this.textBoxTarjeta);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Name = "pagarCompra";
+            this.Text = "Pagar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxTarjeta;
+        private System.Windows.Forms.Button buttonComprar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ PalcoNet/Comprar/pagarCompra.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PalcoNet.Comprar
+{
+    public partial class pagarCompra : Form
+    {
+        private dbmanager db;
+        private string username;
+        private DateTime todayDateTime = DateTime.Now;
+
+        public pagarCompra(dbmanager db, String username)
+        {
+            InitializeComponent();
+            this.db = db;
+            this.username = username;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                textBoxTarjeta.Enabled = true;
+                comboBox1.Enabled = true;
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                textBoxTarjeta.Enabled = false;
+                comboBox1.Enabled = false;
+                textBox1.Enabled = false;
+
+            }
+        }
+
+        private void buttonComprar_Click(object sender, EventArgs e)
+        {
+            string nroTarjeta = textBoxTarjeta.Text;
+
+            string query = "" +
+                           "" +
+                           "" +
+                           "" +
+                           "" ;
+            
+            if (radioButton2.Checked)
+            {
+                // Pago con tarjeta
+            }
+            else
+            {
+                // Pago efectivo
+            }
+
+            db.Ejecutar(query);
+
+            // summo 10 ptos al cliente
+            query = "";
+
+            db.Ejecutar(query);
+        }
+    }
+}
